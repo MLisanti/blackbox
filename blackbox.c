db9b28b568d8ea14ce3482013bc9ec502ef5712b
Massimo <m_lisanti@hotmail.it>
2023-08-14 12:02:42 +0200

    Atomi
    Quattro atomi; gestione direzione atomi destra, alto

--- blackbox.c
+++ blackbox.c
@@ -25,8 +25,8 @@ typedef struct s_atomo atomo;
 
 int posVettore(int x, int y, int largh);
 void azzeraMat(int* campo, int lungh, int largh);
-void stampaMat(int* , int , int , elettrone, atomo);
-void simulaElettrone(elettrone * e, atomo * a);
+void stampaMat(int* , int , int , elettrone, atomo*, int);
+void simulaElettrone(elettrone * e, atomo * vecAtomi, int qta);
 
 int main(){
 	int campo[LATO * LATO];
@@ -39,19 +39,28 @@ int main(){
 		0
 	};
 	
-	atomo at = {
-		{5,4} // pos
-	};
+	atomo arrAtomi[4];
 	
-	stampaMat(campo,LATO, LATO, elProva, at);
+	for(int iA = 0 ; iA<4; iA++) {
+		arrAtomi[iA].pos.x = -1;
+		arrAtomi[iA].pos.y = -1;
+	}
+	
+	arrAtomi[0].pos.x = 5;
+	arrAtomi[0].pos.y = 4;
+	
+	arrAtomi[1].pos.x = 5;
+	arrAtomi[1].pos.y = 1;
+	
+	stampaMat(campo,LATO, LATO, elProva, arrAtomi, 4);
 	
 	for(int i=0;i<LATO;i++) {
 		
 		printf("Press Enter to step...\n");
 		getchar();
 		system("cls");
-		simulaElettrone(&elProva, &at);
-		stampaMat(campo,LATO, LATO, elProva, at);
+		simulaElettrone(&elProva, arrAtomi, 4);
+		stampaMat(campo,LATO, LATO, elProva, arrAtomi, 4);
 	}
 	
 	
@@ -69,10 +78,10 @@ void azzeraMat(int* campo, int lungh, int largh){
 	}
 }
 
-void stampaMat(int* campo, int lungh, int largh, elettrone elett, atomo at){
+void stampaMat(int* campo, int lungh, int largh, elettrone elett, atomo* vecAtomi, int qtaAtomi){
 	char valoreCampo;
 	int x, y;
-	
+	int a = 0;
 	for(y=0; y<lungh; y++){
 		for(x=0; x<largh; x++){
 			valoreCampo = campo[posVettore(x, y, largh)];
@@ -80,8 +89,10 @@ void stampaMat(int* campo, int lungh, int largh, elettrone elett, atomo at){
 			if(elett.pos.x == x && elett.pos.y == y) 
 				valoreCampo = 1;
 				
-			if(at.pos.x == x && at.pos.y == y) 
-				valoreCampo = 5;
+			for(a = 0; a< qtaAtomi; a++) {
+				if(vecAtomi[a].pos.x == x && vecAtomi[a].pos.y == y) 
+					valoreCampo = 5;
+			}
 				
 			printf("|%3d ", valoreCampo);
 		}
@@ -93,21 +104,56 @@ void stampaMat(int* campo, int lungh, int largh, elettrone elett, atomo at){
 	}
 }
 
-void simulaElettrone(elettrone * e, atomo * a) {
+void simulaElettrone(elettrone * e, atomo* vecAtomi, int qtaAtomi) {
+	
+	int iAtomo;
+	
+	int altoDestra  = 0;
+	int bassoDestra = 0;
+	int altoSinistra  = 0;
+	int bassoSinistra = 0;
+	
+	for(iAtomo = 0; iAtomo<qtaAtomi; iAtomo++) {
+		atomo* a = &vecAtomi[iAtomo];
+		
+		if(altoDestra == 0 && (a->pos.x == e->pos.x + 1 && a->pos.y == e->pos.y - 1))
+			altoDestra = 1;
+		if(bassoDestra == 0 && (a->pos.x == e->pos.x + 1 && a->pos.y == e->pos.y + 1))
+			bassoDestra = 1;
+		
+		if(altoSinistra == 0 && (a->pos.x == e->pos.x - 1 && a->pos.y == e->pos.y - 1))
+			altoSinistra = 1;
+		if(bassoSinistra == 0 && (a->pos.x == e->pos.x - 1 && a->pos.y == e->pos.y + 1))
+			bassoSinistra = 1;
+	}
+	
 	if(e->dir.x == 1 && e->dir.y == 0) {
 		//verso destra
-		int altoDestra  = a->pos.x == e->pos.x + 1 && a->pos.y == e->pos.y - 1;
-		int bassoDestra = a->pos.x == e->pos.x + 1 && a->pos.y == e->pos.y + 1;
 		
-		if(altoDestra && bassoDestra) {	//due atomi diversi
+		if(altoDestra && bassoDestra) {
 			e->dir.x = -1;
 			e->dir.y = 0;
-		} else if(altoDestra) {
+		} else if(altoDestra && !bassoDestra) {
 			e->dir.x = 0;
 			e->dir.y = 1;
-		} else if(bassoDestra) {
+		} else if(!altoDestra && bassoDestra) {
 			e->dir.x = 0;
 			e->dir.y = -1;
+		}
+	}
+	
+	if(e->dir.x == 0 && e->dir.y == -1) {
+		//verso alto
+		
+		if(altoDestra && altoSinistra) {
+			e->dir.x = 0;
+			e->dir.y = -1;
+		} else if(altoDestra && !altoSinistra) {
+			e->dir.x = -1;
+			e->dir.y = 0;
+		} else if(!altoDestra && altoSinistra) {
+			e->dir.x = 1;
+			e->dir.y = 0;
 		}
 	}
 	
